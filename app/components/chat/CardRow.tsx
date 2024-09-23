c02cbb85e1318e8184f7ae77f1cee626a7570951
cheonghaLim <thddns12369@naver.com>
2024-09-23 20:12:27 +0900

    chat-room-component udpate

--- app/components/chat/CardRow.tsx
+++ app/components/chat/CardRow.tsx
@@ -2,8 +2,15 @@
 import { useState } from "react";
 import Link from "next/link";
 
+interface CardRowProps {
+  roomId: string;
+  name: string;
+  userCount: number;
+  unReadMessageCount: number;
+}
+
 //Path variable 로 받은 id 값으로 연결하기 
-export default function CardRow() {
+export default function CardRow({ roomId, name, userCount, unReadMessageCount }: CardRowProps) {
   const [id, setId] = useState();
 
   return (
@@ -11,18 +18,19 @@ export default function CardRow() {
       <div className="max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800">
         <Link href="/chats">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            방 제목
+          {name}
           </h5>
         </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          채팅방 간략 소개
+        {userCount}명이 참여 중
+        읽지 않은 메시지 {unReadMessageCount}개
         </p>
         <Link
         // href 주소에 link로 변경하기 
           href="/chats"
           className="inline-flex items-center rounded-lg bg-green-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
         >
-          참여하기
+          입장하기
           <svg
             className="ms-2 size-3.5 rtl:rotate-180"
             aria-hidden="true"
