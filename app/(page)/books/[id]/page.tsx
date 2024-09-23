4e31bb159472a5b0193f748aa729ed6c518315c4
cheonghaLim <thddns12369@naver.com>
2024-09-23 15:44:36 +0900

    bookDetails update

--- app/(page)/books/[id]/page.tsx
+++ app/(page)/books/[id]/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import BookDetails from "@/app/components/BookDetails"
+import { useParams } from "next/navigation"
 
 export default function Books() {
+  const params = useParams();
+  const id = params?.id as string;
+
   return (
     <div>
-      <BookDetails />
+      <BookDetails bookId={id}/>
     </div>
   )
 }
