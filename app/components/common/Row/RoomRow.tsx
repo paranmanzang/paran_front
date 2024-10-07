4a807ae37ff06931e08d6f7a34e044a15ad1b6e0
Jyservice781 <gimjuyeong512@gmail.com>
2024-10-07 16:59:33 +0900

    update

--- app/components/common/Row/RoomRow.tsx
+++ app/components/common/Row/RoomRow.tsx
@@ -4,96 +4,75 @@ import { getRooms, saveCurrentRoom, saveLoading } from "@/lib/features/room/room
 import { useRouter } from "next/navigation";
 import { RoomModel } from "@/app/model/room/room.model";
 import { useAppDispatch } from "@/lib/store";
-import { FileType } from "@/app/model/file/file.model";
 import { useSelector } from "react-redux";
+import { FileType } from "@/app/model/file/file.model";
 import Link from "next/link";
 import Image from "next/image";
 import { roomService } from "@/app/service/room/room.service";
 import { fileService } from "@/app/service/File/file.service";
 import { getFiles, saveCurrentFile, upLoading } from "@/lib/features/file/file.slice";
-import Pagination from "./pagination/Pagination";
+
 interface RoomRowProps {
   active: boolean;
   onSelect: () => void;
 }
 
-interface RoomFile {
-  refId: number;
-  path: string;
-}
-
 const RoomRow = ({ active, onSelect }: RoomRowProps) => {
   const [isActive, setIsActive] = useState<boolean>(active);
-  const rooms = useSelector(getRooms) as RoomModel[];
-  const files = useSelector(getFiles) as { roomFiles: RoomFile[] };
+  const rooms = useSelector(getRooms);
+  const files = useSelector(getFiles);
   const dispatch = useAppDispatch();
   const router = useRouter();
 
-  const [page, setPage] = useState<number>(1);
-  const [pageSize, setPageSize] = useState<number>(25);
-  // total 만 바꿔주면 됨
-  const totalItems = 100;
+  // 페이지네이션 정보
+  const size: number = 25;
+  const [page, setPage] = useState<number>(0);
 
   useEffect(() => {
     setIsActive(active);
 
-    const loadData = async () => {
-      try {
-        dispatch(saveLoading(true));
-        await roomService.findByEnabled(page, pageSize, dispatch);
-        if (rooms.length > 0) {
-          await loadRoomFiles(rooms);
-        }
-      } catch (error) {
-        console.error("데이터 로딩 중 오류 발생:", error);
-      } finally {
-        dispatch(saveLoading(false));
-      }
+    const loadRoomsAndFiles = async () => {
+      await roomService.findByEnabled(page, size, dispatch);
+      loadRoomFiles(rooms);
+      dispatch(saveLoading(false));
     };
 
-    loadData();
-  }, [active, dispatch, page, pageSize]);
+    loadRoomsAndFiles();
+  }, [active, dispatch, page, rooms]);
 
-  const loadRoomFiles = async (rooms: RoomModel[]) => {
-    if (rooms.length > 0) {
-      const roomIds = rooms.map(room => room.id).filter((id): id is number => id !== undefined);
-      try {
-        dispatch(upLoading(true));
-        await fileService.selectFileList(roomIds, FileType.ROOM, dispatch);
-      } catch (error) {
-        console.error("파일 로딩 중 오류 발생:", error);
-      }
-    }
-  }
+  const loadRoomFiles = async (rooms: any[]) => {
+    const roomIds = rooms.map((room) => room.id);
+    await fileService.selectFileList(roomIds, FileType.ROOM, dispatch);
+    dispatch(upLoading(false));
+  };
 
-  const getRoomImage = (roomId: number | undefined): string => {
-    if (roomId !== undefined && files.roomFiles) {
-      const roomFile = files.roomFiles.find(file => file.refId === roomId);
-      return roomFile?.path 
-        ? `${process.env.NEXT_PUBLIC_FILE_URL}/one?path=${roomFile.path}` 
-        : `${process.env.NEXT_PUBLIC_IMAGE_DEFAULT}`;
+  const handleClick = (): void => {
+    onSelect();
+  };
+
+  // 특정 공간에 맞는 파일을 찾는 함수
+  const getRoomImage = (roomId: number | undefined) => {
+    if (roomId !== undefined) {
+      const roomFile = files.roomFiles.find((file) => file.refId === roomId);
+      return roomFile
+        ? `${process.env.NEXT_PUBLIC_FILE_URL}/one?path=${roomFile.path}`
+        : `${process.env.NEXT_PUBLIC_IMAGE_DEFAULT}`; // 기본 이미지 제공
     }
     return `${process.env.NEXT_PUBLIC_IMAGE_DEFAULT}`;
   };
 
-  const onClickToDetail: React.MouseEventHandler<HTMLDivElement> = (event) => {
-    const currentId = Number(event.currentTarget.dataset.roomId);
-
-    if (!isNaN(currentId)) {
+  const onClickToDetail = (currentId: number | undefined): void => {
+    if (currentId !== undefined) {
       const currentRoom = rooms.find(({ id }) => id === currentId);
       if (currentRoom) {
         dispatch(saveCurrentRoom(currentRoom));
-        const currentFile = files.roomFiles?.find(({ refId }) => refId === currentId) ?? null;
-        dispatch(saveCurrentFile(currentFile) ?? null);
-        router.push(`/rooms/${currentId}`);
-      } else {
-        console.error("Room ID 알 수 없음:", currentId);
+        dispatch(saveCurrentFile(files.roomFiles.find(({ refId }) => refId === currentId) ?? null));
+        router.push(`${process.env.NEXT_PUBLIC_ROOM_URL}/${currentId}`);
       }
     } else {
-      console.error("room ID 찾을 수 없음");
+      console.error("ID is null");
     }
   };
-
 
   return (
     <>
@@ -101,9 +80,9 @@ const RoomRow = ({ active, onSelect }: RoomRowProps) => {
         rooms.map((room: RoomModel) => (
           <div className="relative max-w-sm" key={room.id}>
             <div
-              className={`max-w-sm rounded-lg border border-gray-200 bg-white shadow ${isActive ? 'ring-2 ring-green-500' : ''}`}
-              onClick={onClickToDetail}
-              data-room-id={room.id}
+              className={`max-w-sm rounded-lg border border-gray-200 bg-white shadow ${isActive ? 'ring-2 ring-green-500' : ''
+                }`}
+              onClick={handleClick}
             >
               <Link href={`/rooms/${room.id}`} passHref>
                 <Image
@@ -118,7 +97,10 @@ const RoomRow = ({ active, onSelect }: RoomRowProps) => {
 
               <div className="p-5">
                 <Link href={`/rooms/${room.id}`}>
-                  <h5 className={`mb-2 text-lg font-medium tracking-tight ${isActive ? 'text-green-600' : 'text-gray-900'}`}>
+                  <h5
+                    className={`mb-2 text-lg font-medium tracking-tight ${isActive ? 'text-green-600' : 'text-gray-900'
+                      }`}
+                  >
                     {room.name}
                   </h5>
                 </Link>
@@ -127,12 +109,11 @@ const RoomRow = ({ active, onSelect }: RoomRowProps) => {
                 </p>
                 <p className="text-sm font-medium">판매자: {room.nickname}</p>
                 <button
-                  type="button"
-                  onClick={onClickToDetail}
-                  data-room-id={room.id}
-                  className={`mt-5 inline-flex w-full items-center rounded-lg p-3 text-sm font-medium text-white ${
-                    isActive ? 'bg-green-600 hover:bg-green-700' : 'bg-green-400 hover:bg-green-500'
-                  }`}
+                  onClick={() => onClickToDetail(room.id)}
+                  className={`mt-5 inline-flex w-full items-center rounded-lg p-3 text-sm font-medium text-white ${isActive
+                    ? 'bg-green-600 hover:bg-green-700'
+                    : 'bg-green-400 hover:bg-green-500'
+                    }`}
                 >
                   상세보기
                   <svg
@@ -158,13 +139,6 @@ const RoomRow = ({ active, onSelect }: RoomRowProps) => {
       ) : (
         <div>등록된 공간이 없습니다.</div>
       )}
-       <Pagination 
-        currentPage={page} 
-        pageSize={pageSize} 
-        totalItems={totalItems} 
-        onPageChange={setPage} 
-        onPageSizeChange={setPageSize} 
-      />
     </>
   );
 };
