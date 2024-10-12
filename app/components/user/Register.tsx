956df4bdbb42a3973ec22a72752380bbe224f4ea
SongJIH <jh2song@naver.com>
2024-10-12 17:04:05 +0900

    register

--- app/components/user/Register.tsx
+++ app/components/user/Register.tsx
@@ -1,22 +1,24 @@
 "use client"
+import { userService } from "@/app/service/user/user.service";
+import { useAppDispatch } from "@/lib/store";
 import { ChangeEvent, useState } from "react";
 
 interface FormData {
-username: string;
-password: string;
-repeat_password: string;
-nickname: string;
-tel: string;
-name: string;
+  username: string;
+  password: string;
+  passwordcheck: string;
+  nickname: string;
+  tel: string;
+  name: string;
 }
 
-export default function register() {
-  
+export default function Register() {
+  const dispatch = useAppDispatch();
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [formData, setFormData] = useState<FormData>({
     username: '',
     password: '',
-    repeat_password: '',
+    passwordcheck: '',
     nickname: '',
     tel: '',
     name: ''
@@ -31,19 +33,20 @@ export default function register() {
   };
 
   const onSubmit = () => {
-    console.log(formData); 
+    console.log(formData);
+    userService.insertUser(formData, dispatch);
   };
 
   return (
     <div className="my-6 py-40">
       <div className="mx-auto max-w-md">
-      <h1 className="text-2xl mb-6">파란만장 서비스와 함께해요!</h1>
+        <h1 className="mb-6 text-2xl">파란만장 서비스와 함께해요!</h1>
         <div className="group relative z-0 mb-5 w-full">
           <input
             type="text"
             name="username"
             id="username"
-            className="border peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900"
+            className="peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900"
             placeholder=""
             onChange={handleChange}
             required
@@ -61,7 +64,7 @@ export default function register() {
             name="password"
             id="password"
             onChange={handleChange}
-            className="border peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-400 focus:outline-none focus:ring-0"
+            className="peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-400 focus:outline-none focus:ring-0"
             placeholder=" "
             required
           />
@@ -76,16 +79,16 @@ export default function register() {
         <div className="group relative z-0 mb-5 w-full">
           <input
             type="password"
-            name="repeat_password"
-            id="repeat_password"
+            name="passwordcheck"
+            id="passwordcheck"
             onChange={handleChange}
-            className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-400 peer"
+            className="peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-400 focus:outline-none focus:ring-0"
             placeholder=" "
             required
           />
           <label
-            htmlFor="repeat_password"
-            className="absolute text-sm text-gray-500 duration-300 -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-green-400  peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+            htmlFor="passwordcheck"
+            className="absolute start-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 bg-white px-2 text-sm text-gray-500 duration-300 peer-focus:top-2  peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-green-400 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
           >
             비밀번호 확인
           </label>
@@ -94,8 +97,8 @@ export default function register() {
           <input
             type="text"
             name="nickname"
-            id="nickname"onChange={handleChange}
-            className="border peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-400 focus:outline-none focus:ring-0 "
+            id="nickname" onChange={handleChange}
+            className="peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-400 focus:outline-none focus:ring-0 "
             required
           />
           <label
@@ -112,7 +115,7 @@ export default function register() {
             name="tel"
             id="tel"
             onChange={handleChange}
-            className="border peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-400 focus:outline-none focus:ring-0 "
+            className="peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-400 focus:outline-none focus:ring-0 "
             placeholder=" "
             required
           />
@@ -127,8 +130,8 @@ export default function register() {
           <input
             type="text"
             name="name"
-            id="name"onChange={handleChange}
-            className="border peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-400 focus:outline-none focus:ring-0 "
+            id="name" onChange={handleChange}
+            className="peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-400 focus:outline-none focus:ring-0 "
             placeholder=" "
             required
           />
