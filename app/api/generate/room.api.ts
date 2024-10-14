d63fa143f897d2f7a4e1b52ddb473395d9174b23
SongJIH <jh2song@naver.com>
2024-10-14 21:48:09 +0900

    update likeRoom

--- app/api/generate/room.api.ts
+++ app/api/generate/room.api.ts
@@ -3,20 +3,23 @@ import requests from "@/app/api/requests";
 import api from "@/app/api/axios";
 
 export const roomAPI = {
-    insert(roomModel: RoomModel) { return api.post<RoomModel>(`${requests.fetchRooms}`, roomModel)},
+    insert(roomModel: RoomModel) { return api.post<RoomModel>(`${requests.fetchRooms}`, roomModel) },
 
-    modify(roomModel: RoomUpdateModel) { return api.put<RoomModel>(`${requests.fetchRooms}`, roomModel)},
+    modify(roomModel: RoomUpdateModel) { return api.put<RoomModel>(`${requests.fetchRooms}`, roomModel) },
 
     drop(id: number) { return api.delete<boolean>(`${requests.fetchRooms} + /${id}`) },
 
-    findByUser(nickname: string, page: number, size: number) { return api.get<Page<RoomModel>>(`${requests.fetchRooms}` + `/user`, { params: { nickname, page, size }})},
+    findByUser(nickname: string, page: number, size: number) { return api.get<Page<RoomModel>>(`${requests.fetchRooms}` + `/user`, { params: { nickname, page, size } }) },
 
-    findAll(page: number, size: number) { return api.get<Page<RoomModel>>(`${requests.fetchRooms}`, { params: { page, size } })},
+    findAll(page: number, size: number) { return api.get<Page<RoomModel>>(`${requests.fetchRooms}`, { params: { page, size } }) },
 
-    findByEnabled(page: number, size: number) { return api.get<Page<RoomModel>>(requests.fetchRooms + '/enabled', { params: { page, size } })},
+    findByEnabled(page: number, size: number) { return api.get<Page<RoomModel>>(requests.fetchRooms + '/enabled', { params: { page, size } }) },
 
-    modifyConfirm(id: number) { return api.put<RoomModel>(requests.fetchRooms + `/confirm/${id}`)},
+    modifyConfirm(id: number) { return api.put<RoomModel>(requests.fetchRooms + `/confirm/${id}`) },
 
-    findTime(roomId: number) { return api.get<TimeModel[]>(requests.fetchRooms + `/times/${roomId}`)},
+    findTime(roomId: number) { return api.get<TimeModel[]>(requests.fetchRooms + `/times/${roomId}`) },
 
+    findLikeRoomList: (nickname: string) => {
+        return api.get<RoomModel[]>(requests.fetchRooms + `/like/${nickname}`);
+    },
 }
