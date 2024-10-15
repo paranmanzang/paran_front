ad5268239a69feb33c990c5384d8bf6d1dd5134c
SongJIH <jh2song@naver.com>
2024-10-15 10:06:35 +0900

    update liked room

--- app/service/users/likeRoom.service.ts
+++ app/service/users/likeRoom.service.ts
@@ -1,16 +1,17 @@
 import { AppDispatch } from "@/lib/store";
-import { addLikedRoom, deleteLikedRoom, saveLikedRooms } from "@/lib/features/users/likeRoom.slice";
 import { saveError, saveLoading } from "@/lib/features/users/user.slice";
 import { LikeRoomModel } from "@/app/model/user/users.model";
 import { likeRoomAPI } from "@/app/api/generate/likeRoom.api";
+import { addLikedRoom, removeLikedRoom } from "@/lib/features/room/room.slice";
+import { RoomModel } from "@/app/model/room/room.model";
 
-const insert = async (likeRoomModel: LikeRoomModel, dispatch: AppDispatch): Promise<void> => {
+const insert = async (likeRoomModel: LikeRoomModel, dispatch: AppDispatch, room: RoomModel): Promise<void> => {
     try {
         dispatch(saveLoading(true));
         const response = await likeRoomAPI.insert(likeRoomModel)
         console.log("공간 좋아요: ", response.data)
         if ('id' in response.data && 'nickname' in response.data) {
-            dispatch(addLikedRoom(response.data))
+            dispatch(addLikedRoom(room))
         }
     } catch (error) {
         dispatch(saveError("좋아요 중 오류 발생했습니다."));
@@ -25,8 +26,9 @@ const drop = async (likeRoomModel: LikeRoomModel, dispatch: AppDispatch): Promis
     try {
         dispatch(saveLoading(true));
         const response = await likeRoomAPI.drop(likeRoomModel)
-        if (likeRoomModel.id !== undefined) {
-            dispatch(deleteLikedRoom(likeRoomModel.id));
+
+        if (likeRoomModel.id !== undefined && response.data) {
+            dispatch(removeLikedRoom(likeRoomModel.roomId));
         }
     } catch (error) {
         dispatch(saveError("좋아요 취소 중 오류 발생했습니다."));
@@ -36,7 +38,6 @@ const drop = async (likeRoomModel: LikeRoomModel, dispatch: AppDispatch): Promis
     }
 };
 
-// 좋아요 마이페이지 확인
 
 export const likeRoomService = {
     insert,
